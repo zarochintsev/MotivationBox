7ba938f0e6a3707803f55ce39537d4e4b681a058
Alex Zarochintsev <alex.zarochintsev@gmail.com>
2017-09-24 14:12:18 +0300

    Get motivation with service

--- Motivation/Classes/PresentationLayer/Modules/Motivations/Module/Interactor/MotivationsInteractor.swift
+++ Motivation/Classes/PresentationLayer/Modules/Motivations/Module/Interactor/MotivationsInteractor.swift
@@ -28,14 +28,14 @@ import UIKit
 
 class MotivationsInteractor {
     
-    // MARK: - Constants
-    
-    fileprivate struct C {
-        static let startIndex = 89
-    }
+    // MARK: - General
     
     /// Reference to the Presenter's output interface.
     weak var output: MotivationsInteractorOutput!
+    
+    // MARK: - Services
+    
+    var motivationService: MotivationService!
 }
 
 // MAKR: - MotivationsInteractorInput
@@ -43,21 +43,10 @@ class MotivationsInteractor {
 extension MotivationsInteractor: MotivationsInteractorInput {
     
     func requestMotivations() {
-        let day = Calendar(identifier: Calendar.Identifier.iso8601).ordinality(of: .day, in: .year, for: Date())!
-        
-        var elements: [Motivation] = []
-        
-        for index in C.startIndex...day {
-            let title = "\(LS.Common.day.localized()) \(index)"
-            let message = "\(index)".localized()
-            
-            let motivation = Motivation(title: title, message: message)
-            
-            elements.append(motivation)
+        motivationService.requestMotivations { [weak self] (elements: [Motivation]) in
+            guard let weakSelf = self else { return }
+            weakSelf.output.updatedMotivations(elements)
         }
         
-        elements.append(Motivation(title: "\(LS.Common.day.localized()) \(day+1)", message: LS.Motivations.new.localized()))
-        
-        output.updatedMotivations(elements)
     }
 }
